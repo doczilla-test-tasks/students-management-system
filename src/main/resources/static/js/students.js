ab19f666fbc3583a0b321ed73fca4e4f0cbc39c3
Vladislav Harlamov <v.harlamov@astondevs.ru>
2024-12-19 20:06:30 +0300

    Create UI skeleton with functionality to view students list and delete specific student

---
+++ src/main/resources/static/js/students.js
@@ -0,0 +1,96 @@
+function getStudentsBatchUrl(limit, offset) {
+    baseUrl = "http://localhost:4567/api/v1/students";
+    return baseUrl + "?limit=" + limit + "&offset=" + offset;
+}
+
+function deleteStudentByIdUrl(id) {
+    baseUrl = "http://localhost:4567/api/v1/students";
+    return baseUrl + "/delete/" + id;
+}
+
+function getStudentsBatch(limit, offset) {
+    $.get({
+        url: getStudentsBatchUrl(limit, offset),
+        dataType: "json",
+        success: function(students) {
+            
+            $("#students-table tbody").html("");
+            $.each(students, function (idx, student) { 
+                $("#students-table tbody").append(
+                    $("<tr></tr>")
+                        .data("student", student)
+                        .append(
+                            $("<td></td>").append(
+                                offset + idx + 1
+                            ),
+                            $("<td></td>").append(student["surname"]),
+                            $("<td></td>").append(student["name"]),
+                            $("<td></td>").append(student["patronymic"] ?? "-"),
+                            $("<td></td>").append(new Date(student["birthDate"]).toLocaleDateString("ru-RU")),
+                            $("<td></td>").append(student["group"]),
+                            $("<input></input>", { "class": "delete-button", "name": "delete", "type": "button"}).val("Delete")
+                        )
+                );
+            });
+
+            $("input.delete-button:button").click(function(e) {
+                var deletingStudentId = $(this).parent().data("student").id;
+                $.ajax({
+                    url: deleteStudentByIdUrl(deletingStudentId),
+                    dataType: "json",
+                    method: "DELETE",
+                    global: true,
+                    success: function(deletionStatus) {
+                        console.log("Student with id " + deletingStudentId + " deletion status: " + deletionStatus);
+                    },
+                    error: function(err) {
+                        console.log(err);
+                    }
+                });
+            });
+            
+            start = offset + 1;
+            end = offset + limit + 1;
+            $(".table-description").html(
+                $("<p></p>").append(start + " to " + end)
+            );
+        },
+        error: function(err) {
+            console.log(err);
+        }
+    });
+}
+
+$(document).ready(function () {
+    $("#students-table").data("offset", 0);
+    getStudentsBatch(parseInt($("input:radio[name='limit']:checked").val()), $("#students-table").data("offset"));
+   
+    $(document).on("ajaxComplete", function(e, xhr, opts) {
+        var getStudentsBatchUrlRegex = new RegExp(".*\/api\/v1\/students\\?limit=\\d+&offset=\\d+");
+        if ((!getStudentsBatchUrlRegex.test(opts.url))) {
+            console.log("Resend request for table data");
+            getStudentsBatch(parseInt($("input:radio[name='limit']:checked").val()), $("#students-table").data("offset"));
+        }
+    });
+
+    $("input:radio[name='limit']").on("change", function() {
+        getStudentsBatch(parseInt($(this).val()), $("#students-table").data("offset"));
+    });
+
+    $("input:button[name='next']").click(function(e) {
+        var cur_offset = $("#students-table").data("offset");
+        $("#students-table").data("offset", cur_offset + parseInt($("input:radio[name='limit']:checked").val()));
+        getStudentsBatch(parseInt($("input:radio[name='limit']:checked").val()), $("#students-table").data("offset"));
+    });
+
+    $("input:button[name='prev']").click(function(e) {
+            var cur_offset = $("#students-table").data("offset");
+            var cur_limit = parseInt($("input:radio[name='limit']:checked").val());
+            if (cur_offset >= cur_limit) {
+                $("#students-table").data("offset", cur_offset - cur_limit);
+            } else {
+                console.log("Unable to find previous page");
+            }
+            getStudentsBatch(parseInt($("input:radio[name='limit']:checked").val()), $("#students-table").data("offset"));
+        });
+});
